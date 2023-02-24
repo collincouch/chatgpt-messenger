936e06bb90cd5894399da2bdc1d329d5731403f2
collincouch <collindcouch@gmail.com>
2023-02-24 10:54:05 -0700

    updated aligment on login screen

--- components/Login.tsx
+++ components/Login.tsx
@@ -3,7 +3,7 @@ import { signIn } from "next-auth/react";
 import Image from "next/image";
 export default function Login() {
   return (
-    <div className="bg-[#11A37F] h-screen flex flex-col items-center">
+    <div className="bg-[#11A37F] h-screen flex flex-col items-center justify-center">
       <Image
         src="https://links.papareact.com/2i6"
         width={300}
@@ -14,8 +14,14 @@ export default function Login() {
         onClick={() => signIn("google")}
         className="text-white font-bold text-3xl animate-pulse"
       >
-        Sign In to use ChatGPT
+        Sign In to use my ChatGPT clone
       </button>
+      <p className="text-sm">
+        developed by{" "}
+        <a href="https://cctechfactory.vercel.app/" className="underline">
+          collin couch
+        </a>
+      </p>
     </div>
   );
 }
